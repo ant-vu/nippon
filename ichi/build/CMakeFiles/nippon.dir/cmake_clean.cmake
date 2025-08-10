c530977e57cd140ea664dbf1591a41849f49bb53
Anthony Vu <70167429+ant-vu@users.noreply.github.com>
2025-08-10 23:55:57 +0000

    feat: cmake -S . -B build/

---
+++ ichi/build/CMakeFiles/nippon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nippon.dir/src/main.cpp.o"
+  "CMakeFiles/nippon.dir/src/main.cpp.o.d"
+  "nippon"
+  "nippon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nippon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ichi/build/CMakeFiles/nippon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nippon.
+# This may be replaced when dependencies are built.
